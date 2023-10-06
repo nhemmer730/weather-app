743d9eba4ddf601d2e76276e774bdf99a2eeb1c1
nhemmer730 <146217891+nhemmer730@users.noreply.github.com>
2023-10-06 12:30:44 -0400

    Added icon to API

--- src/index.js
+++ src/index.js
@@ -15,22 +15,26 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = (response.data.name);
     descriptionElement.innerHTML = (response.data.weather[0].description);
     humidityElement.innerHTML = (response.data.main.humidity);
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "d5dd31649081791d17cdb524d42767c1";
-let city = "Paris";
+let city = "Boston";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
