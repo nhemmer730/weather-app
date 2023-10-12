b985101afbf6f2251a9046bdf3ca0beb60a3e1b0
nhemmer730 <146217891+nhemmer730@users.noreply.github.com>
2023-10-12 12:21:14 -0400

    added HTML forecast and Css properties

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`
 }
 
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -32,7 +33,7 @@ function displayTemperature(response) {
     humidityElement.innerHTML = (response.data.main.humidity);
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
