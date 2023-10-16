4b9db356ddf333d9fcdf6532f4ea53438bdef697
nhemmer730 <146217891+nhemmer730@users.noreply.github.com>
2023-10-16 12:23:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,18 +14,19 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
     let days = ["Thu", "Fri", "Sat", "Sun"];
-    days.forEach(function (day) {
+     forecast.forEach(function (forecastDay) {
     
     forecastHTML =
         forecastHTML + 
-        `
+        `        
         <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${forecastDay.dt}</div>
           <img src="http://openweathermap.org/img/wn/50d@2x.png" 
            alt="" 
            width="40"
@@ -45,7 +46,7 @@ function displayForecast() {
 function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "d5dd31649081791d17cdb524d42767c1";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}& units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}& units=metric`;
     console.log(apiUrl);
 }
 function displayTemperature(response) {
